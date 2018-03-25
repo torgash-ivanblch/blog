09654b63b50612ce4ede53bcc3c2ac16cabdb6e4
torgash <torgash@outlook.com>
2018-03-25 21:21:26 +0300

    some fixes

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,11 +1,14 @@
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
+from django.contrib.sites.models import Site
 from .models import Post
 
+domain = Site.objects.get_current().domain
+
 class LatestPostsFeed(Feed):
-    title = 'My Blog'
+    title = 'VOKINA BLOG'
     link = '/blog'
-    description = 'New posts of my blog.'
+    description = 'Последние публикации'
 
     def items(self):
         return Post.published.all()[:5]
@@ -15,3 +18,10 @@ class LatestPostsFeed(Feed):
 
     def item_description(self, item):
         return truncatewords(item.body, 30)
+
+  
+"""
+Returns an extra keyword arguments dictionary that is used with
+the `add_item` call of the feed generator.
+Add the 'content' field of the 'Entry' item, to be used by the custom feed generator.
+"""
